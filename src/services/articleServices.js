f2df9530242891dde1a88fae74f35912c10566bc
Jeong Pak <dannypak716@gmail.com>
2021-11-12 15:49:53 -0500

    articles display after authentication. edit and delete functionality works

--- src/services/articleServices.js
+++ src/services/articleServices.js
@@ -1,7 +1,15 @@
+import axiosWithAuth from "./../utils/axiosWithAuth";
 
-
-const articleService = ()=> {
-}
+const articleService = (stateSetter) => {
+  axiosWithAuth()
+    .get("http://localhost:5000/api/articles")
+    .then((response) => {
+      return stateSetter(response.data);
+    })
+    .catch((error) => {
+      console.error("COULD NOT FETCH ARTICLES!", error);
+    });
+};
 
 export default articleService;
 
